495e4ad9701b49e6f7b1b91bced4ed6ac3000253
Boris Stroganov <k20040031@kcl.ac.uk>
2022-12-05 21:00:16 +0000

    E1S5 Add Temp Text to Speech Tab

--- client/src/SpeechTab.tsx
+++ client/src/SpeechTab.tsx
@@ -4,6 +4,18 @@ function SpeechTab() {
     return (
         <div className="speech-tab">
             <h1>Speech Capture</h1>
+            <div className="speech-text-wrapper">
+                <h3 className="title">Please Read the Following Text:</h3>
+                <div className="speech-text">
+                    Once upon a time, there lived a dragon. He lived in a cave deep in the forest and was very proud of his home.
+                    One day, the dragon decided to explore the world outside of his cave. He flew high into the sky and
+                    saw many wonderful things. He saw mountains, rivers, and forests. He even saw a castle in the distance.
+                    He was so excited that he decided to fly closer to the castle. As he got closer,
+                    he noticed that the castle was surrounded by a large wall. He flew around the wall and noticed a small opening.
+                    He flew through the opening and landed in the castle courtyard.
+                </div>
+            </div>
+            <div className="speech-capture"></div>
         </div>
     )
 }
