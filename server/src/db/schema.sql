b30b42a93acf6175a70197eede5af197aba4fc4d
Boris Stroganov <k20040031@kcl.ac.uk>
2023-01-19 21:21:42 +0000

    E2S1 Add basic db backend

---
+++ server/src/db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS user (
+    id   TEXT NOT NULL,
+    name TEXT NOT NULL,
+    age  INTEGER NOT NULL,
+
+    PRIMARY KEY (id)
+);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,63 @@
 import express, { Request, Response } from "express";
-const app = express()
+import bodyParser from "body-parser";
+import Database from "better-sqlite3";
+import { v4 as uuidv4 } from 'uuid';
+import fs from "fs";
 
-const PORT = 5000;
+const app = express();
+app.use(bodyParser.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("accessible health dashboard");
-})
+const db = new Database("./src/db/data.db", { verbose: console.log });
+db.pragma("foreign_keys = ON");
 
-app.listen(PORT);
+function query<T>(sql: string, params: Array<any>): Array<T> {
+    return db.prepare(sql).all(params);
+}
+function exec(sql: string, params: Array<any>): void {
+    db.prepare(sql).run(params);
+}
+
+function createTables(): void {
+    const schema = fs.readFileSync("./src/db/schema.sql", {
+        encoding: 'utf8',
+        flag: 'r',
+    });
+    exec(schema, []);
+}
+
+function createUser(name: string, age: number): void {
+    exec(`
+        INSERT INTO user (id, name, age)
+        VALUES (?, ?, ?);
+    `, [uuidv4(), name, age]);
+}
+
+function listUsers(age: number): Array<string> {
+    const users = query<{id: string, name: string, age: number}>(`
+        SELECT *
+        FROM user
+        WHERE age > ?;
+    `, [age]);
+    return users.map(user => user.name);
+}
+
+app.post("/signup", (req: Request, res: Response) => {
+    const {
+        name,
+        age
+    } = req.body;
+    createUser(name, parseInt(age));
+    res.send("created: " + name);
+});
+
+app.get("/list", (req: Request, res: Response) => {
+    const {
+        age
+    } = req.query;
+    const users = listUsers(parseInt(age as string));
+    res.json(users);
+});
+
+createTables();
+
+app.listen(5000);
