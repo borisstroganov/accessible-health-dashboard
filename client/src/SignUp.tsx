eaf1d44b84bdcb538acd870277ad6d2d16c36651
Boris Stroganov <k20040031@kcl.ac.uk>
2023-01-26 17:13:06 +0000

    Add frontend verification to sign up

--- client/src/SignUp.tsx
+++ client/src/SignUp.tsx
@@ -10,6 +10,11 @@ function SignUp({ onClick, onBackClick }: SignUpProps) {
     const [emailValue, setEmailValue] = useState("");
     const [nameValue, setNameValue] = useState("");
     const [passwordValue, setPasswordValue] = useState("");
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
+    const lengthRegex = /^.{8,}$/;
+    const uppercaseRegex = /^(?=.*[A-Z])/;
+    const lowercaseRegex = /^(?=.*[a-z])/;
+    const numbersRegex = /^(?=.*\d)/;
 
     return (
         <div className="SignUp">
@@ -18,6 +23,7 @@ function SignUp({ onClick, onBackClick }: SignUpProps) {
                     <div className="sign-up-input">
                         <label htmlFor="email">Email:</label>
                         <input autoFocus type="email" name="email" value={emailValue} onChange={(e) => setEmailValue(e.target.value)}></input>
+                        <div className="regex" style={{ "display": emailRegex.test(emailValue) ? "limegreen" : "" }}>Please enter a valid email address.</div>
                     </div>
                     <div className="sign-up-input">
                         <label htmlFor="name">Name:</label>
@@ -26,6 +32,10 @@ function SignUp({ onClick, onBackClick }: SignUpProps) {
                     <div className="sign-up-input">
                         <label htmlFor="password">Password:</label>
                         <input type="password" name="password" value={passwordValue} onChange={(e) => setPasswordValue(e.target.value)}></input>
+                        <div className="regex" style={{ "color": lengthRegex.test(passwordValue) ? "limegreen" : "" }}>Must be at least 8 characters.</div>
+                        <div className="regex" style={{ "color": uppercaseRegex.test(passwordValue) ? "limegreen" : "" }}>Must contain an uppercase letter.</div>
+                        <div className="regex" style={{ "color": lowercaseRegex.test(passwordValue) ? "limegreen" : "" }}>Must contain an lowercase letter.</div>
+                        <div className="regex" style={{ "color": numbersRegex.test(passwordValue) ? "limegreen" : "" }}>Must contain a number.</div>
                     </div>
 
                     <button className="sign-up-button" onClick={() => onClick(emailValue, nameValue, passwordValue)}>Register</button>
